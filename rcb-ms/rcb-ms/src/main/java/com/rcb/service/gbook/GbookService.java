67f07225c67d57a20c8986fa4a7b28985c9a10d2
jxbrhgit <773951640@qq.com>
2017-05-21 22:29:30 -0500

    2017-05-22

--- rcb-ms/rcb-ms/src/main/java/com/rcb/service/gbook/GbookService.java
+++ rcb-ms/rcb-ms/src/main/java/com/rcb/service/gbook/GbookService.java
@@ -8,4 +8,5 @@ public interface GbookService {
 	public int delGbookById(String id); //通过id删除表中一条信息
 	 public int delGbooksById(String... ids);//删除多条
 	 public List<Gbook> findGbookByPage(int page);//查找gbook根据page page:从第几条内容开始
+	 public String findGbookMaxPage();//查找gbook记录的总条数
 }
